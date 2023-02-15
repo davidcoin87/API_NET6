417f275a1b73a321c70cf5c8f261834f02ac87d7
David Ruiz <davidcoin87@gmail.com>
2023-02-14 22:48:15 -0500

    Seccion 4 Clase 29
    Seccion 4 Clase 29

--- WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
+++ WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
@@ -14,10 +14,25 @@ namespace WebApiAutores.Controllers
             _context = context;
         }
 
+        //[HttpGet]//ruta por defecto como metodo Get
+        //[HttpGet("listado")]//ruta modificada api/autores/listado
+        //[HttpGet("/listado")]//ruta directa /listado
+        //public async Task<ActionResult<List<Autor>>> Get()
+        //{
+        //    //return new List<Autor>() {
+        //    //    new Autor() { Id = 1, Nombre = "Felipe" },
+        //    //    new Autor() { Id = 2, Nombre = "Daniel" },
+        //    //    new Autor() { Id = 3, Nombre = "Sandra" }
+        //    //};
+
+        //    return await _context.Autores.Include(x => x.Libros).ToListAsync();
+
+        //}
+        
         [HttpGet]//ruta por defecto como metodo Get
         [HttpGet("listado")]//ruta modificada api/autores/listado
         [HttpGet("/listado")]//ruta directa /listado
-        public async Task<ActionResult<List<Autor>>> Get()
+        public List<Autor> Get()
         {
             //return new List<Autor>() {
             //    new Autor() { Id = 1, Nombre = "Felipe" },
@@ -25,7 +40,7 @@ namespace WebApiAutores.Controllers
             //    new Autor() { Id = 3, Nombre = "Sandra" }
             //};
 
-            return await _context.Autores.Include(x => x.Libros).ToListAsync();
+            return _context.Autores.Include(x => x.Libros).ToList();
 
         }
 
@@ -62,17 +77,46 @@ namespace WebApiAutores.Controllers
             return autor;
         }
 
+        //Metodo asincrono
+        //[HttpGet("{id:int}/{param2=persona}")]//puedo convertir un parametro como opcional agregando ? al final
+        //public async Task<ActionResult<Autor>> GetByDataI(int id, string param2)
+        //{
+        //    var autor = await _context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+
+        //    if (autor == null)
+        //    {
+        //        return NotFound("Id no existe");
+        //    }
+
+        //    return autor;
+        //}
+        
+        //Metodo sincrono retornando objeto especifico
+        //[HttpGet("{id:int}/{param2=persona}")]//puedo convertir un parametro como opcional agregando ? al final
+        //public ActionResult<Autor> GetByDataII(int id, string param2)
+        //{
+        //    var autor = _context.Autores.FirstOrDefault(x => x.Id == id);
+
+        //    if (autor == null)
+        //    {
+        //        return NotFound("Id no existe");
+        //    }
+
+        //    return autor;
+        //}
+        
+        //Metodo sincrono opcion 2, retornado objeto generico
         [HttpGet("{id:int}/{param2=persona}")]//puedo convertir un parametro como opcional agregando ? al final
-        public async Task<ActionResult<Autor>> GetByData(int id, string param2)
+        public IActionResult GetByDataIII(int id, string param2)
         {
-            var autor = await _context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+            var autor = _context.Autores.FirstOrDefault(x => x.Id == id);
 
             if (autor == null)
             {
                 return NotFound("Id no existe");
             }
 
-            return autor;
+            return Ok(autor);
         }
 
         [HttpPost]
